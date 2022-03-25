b5a9565f4e1fb4dd550fb2ea48ff0cd6d70b8c51
C000C000 <corentinbreal@gmail.com>
2022-03-25 11:05:00 +0100

    First Commit

---
+++ guicoSymfony/src/Entity/Films.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FilmsRepository::class)]
+class Films
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $titre;
+
+    #[ORM\Column(type: 'text')]
+    private $realisateur;
+
+    #[ORM\Column(type: 'text')]
+    private $acteur;
+
+    #[ORM\Column(type: 'integer')]
+    private $duree;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $miniature;
+
+    #[ORM\Column(type: 'text')]
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getRealisateur(): ?string
+    {
+        return $this->realisateur;
+    }
+
+    public function setRealisateur(string $realisateur): self
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+
+    public function getActeur(): ?string
+    {
+        return $this->acteur;
+    }
+
+    public function setActeur(string $acteur): self
+    {
+        $this->acteur = $acteur;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getMiniature(): ?string
+    {
+        return $this->miniature;
+    }
+
+    public function setMiniature(?string $miniature): self
+    {
+        $this->miniature = $miniature;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
---
+++ guicoSymfony/src/Entity/User.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $nom;
+
+    #[ORM\Column(type: 'text')]
+    private $prenom;
+
+    #[ORM\Column(type: 'text')]
+    private $mail;
+
+    #[ORM\Column(type: 'integer')]
+    private $age;
+
+    #[ORM\Column(type: 'text')]
+    private $motDePasse;
+
+    #[ORM\Column(type: 'text')]
+    private $role;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->motDePasse;
+    }
+
+    public function setMotDePasse(string $motDePasse): self
+    {
+        $this->motDePasse = $motDePasse;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+}
