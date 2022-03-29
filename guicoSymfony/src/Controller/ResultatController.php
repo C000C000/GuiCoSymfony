a69e1a0f479e9648464f3a8b5dde6867efb6d9e1
Guilhem <45591444+Driftix@users.noreply.github.com>
2022-03-29 09:16:26 +0200

    Category results

--- guicoSymfony/src/Controller/ResultatController.php
+++ guicoSymfony/src/Controller/ResultatController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use MovieApiDto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResultatController extends AbstractController
 {
-    #[Route('/resultat', name: 'app_resultat')]
-    public function index(): Response
+    #[Route('/resultat/{idCategory}', name: 'app_resultat')]
+    public function index($idCategory): Response
     {
+        $movieAppDto = new MovieApiDto();
+        $filmCategories = $movieAppDto->getFilmByCategoryId($idCategory,25);
         return $this->render('resultat/index.html.twig', [
             'controller_name' => 'ResultatController',
+            'movies' => $filmCategories,
         ]);
     }
 }
